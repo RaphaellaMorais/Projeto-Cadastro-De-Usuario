2ef2786e790f12d632248cecf92c469026c3402e
RaphaellaMorais <raphaellakm@gmail.com>
2022-09-02 19:53:31 -0300

    removendo-node-modules

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Página da Liga Acadêmica Feminina de Tecnologia do CESMAC.">
+        <div className='display-4'>Bem-Vindo!</div>
+        <hr />
+        <p className='mb-0'>Conheça nossa liga!</p>    
+    </Main>
